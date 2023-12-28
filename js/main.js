df2f1c7a4f89eb4ccc84a18e64e78c987aaefdb0
Fercho <fernandomatiaspessoa471@gmail.com>
2023-12-28 00:55:11 -0300

    frontend - seccion de bienvenida y articulo principal

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const sidebar = document.getElementById("sidebar")
+
+const btnDesktopSidebar = document.getElementById("btn-desktop-sidebar");
+btnDesktopSidebar.addEventListener("click", () => {
+    sidebar.classList.toggle("toggle-sidebar");
+    btnDesktopSidebar.classList.toggle("toogle-btn-sidebar")
+})
