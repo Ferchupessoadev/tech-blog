76a52e16066d7a537cc47aeeaf6435799e923cc2
Nefasque <Pedro0fici4lDev@gmail.com>
2023-12-29 16:23:05 -0400

    my firts commit

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const sidebar = document.getElementById("sidebar")
-
-const btnDesktopSidebar = document.getElementById("btn-desktop-sidebar");
-btnDesktopSidebar.addEventListener("click", () => {
-    sidebar.classList.toggle("toggle-sidebar");
-    btnDesktopSidebar.classList.toggle("toogle-btn-sidebar")
-})
